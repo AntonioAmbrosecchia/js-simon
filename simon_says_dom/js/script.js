efb71d3f85f7bb358f8f04f7ae952e0eed7165e0
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-02-23 13:34:55 +0100

    add countdown

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,10 +1,30 @@
+// Genero 5 numeri casuali da 1 a 50
 function generaNumeri() {
     let numeri = [];
     for (let i = 0; i < 5; i++) {
-        numeri.push(Math.floor(Math.random() * 5) + 1);
+        numeri.push(Math.floor(Math.random() * 50) + 1);
     }
     return numeri;
 }
 
+// Li faccio apparire in pagina
 let numeriCasuali = generaNumeri();
 document.getElementById("numbers-list").innerHTML = numeriCasuali.join("-");
+
+
+// Creo l'interfaccia del countdown
+let tempo = 30;
+let countdownEl = document.getElementById("countdown");
+let hiddenEl = Document.getElementById("answers-form");
+countdownEl.innerText = "Tempo restante: " + tempo;
+
+// Rendo funzionante il countdown e faccio apparire gli input con i numeri da inserire
+let timer = setInterval(() => {
+    tempo--
+    countdownEl.innerText = "Tempo restante: " + tempo;
+
+    if (tempo === 0) {
+        clearInterval(timer);
+        document.getElementById("answers-form").className = "d-block"
+    }
+}, 50)

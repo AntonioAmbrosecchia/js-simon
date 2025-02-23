96788ba1790958ac4044f242995249169c3beeca
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-02-23 15:10:49 +0100

    add numeri utente

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -9,22 +9,48 @@ function generaNumeri() {
 
 // Li faccio apparire in pagina
 let numeriCasuali = generaNumeri();
-document.getElementById("numbers-list").innerHTML = numeriCasuali.join("-");
+document.getElementById("numbers-list").innerHTML = numeriCasuali.join("--")
 
 
-// Creo l'interfaccia del countdown
+//Creo l'interfaccia del countdown
 let tempo = 30;
 let countdownEl = document.getElementById("countdown");
-let hiddenEl = Document.getElementById("answers-form");
+let hiddenEl = document.getElementById("answers-form");
 countdownEl.innerText = "Tempo restante: " + tempo;
 
-// Rendo funzionante il countdown e faccio apparire gli input con i numeri da inserire
+//Rendo funzionante il countdown e faccio apparire gli input con i numeri da isnerire
 let timer = setInterval(() => {
-    tempo--
+    tempo--;
     countdownEl.innerText = "Tempo restante: " + tempo;
 
     if (tempo === 0) {
         clearInterval(timer);
-        document.getElementById("answers-form").className = "d-block"
+        document.getElementById("answers-form").className = "d-block";
+        document.getElementById("numbers-list").innerText = "";
+        document.getElementById("instructions").innerText = "";
+        countdownEl.innerText = "Inserisci i numeri che ricordi";
     }
-}, 50)
+}, 50);
+
+// Stampo i numeri immessi dall'utente e i numeri che doveva indovinare
+
+
+let numeriUtente = [];
+function salvaNumeri() {
+
+
+
+    for (let i = 1; i < 5; i++) {
+        let numeri = document.querySelector("input" + i).value;
+        numeriUtente.push(numeri);
+
+
+    }
+
+    document.getElementById("instructions").innerText = valori.join("--");
+}
+
+document.querySelector("button").addEventListener("click", numeriUtente);
+
+
+
